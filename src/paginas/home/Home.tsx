6050905a4175331e73bfe19ca53d5b02c7c734c3
PriscillaB <priscillatech@outlook.com>
2024-02-14 14:49:41 -0300

    Atividade com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import'./Home.css'
+
+function Home() {
+    return(
+<>
+<div className = "conteiner">
+<h1 className="titulo">Manipulando o Mundo</h1>
+</div>
+<img className="img" src ="paisagem.jpg"
+alt = "imagem"/>
+</>
+    )
+}
+
+export default Home;
